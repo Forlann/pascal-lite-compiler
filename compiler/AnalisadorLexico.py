8db290afef76fbc6ad61362680f94fb062770438
Kaue Forlan Barros de Jesus Souza <2202416@salas.aulas>
2026-03-13 21:37:08 -0300

    Adicionado handler de digito e string

--- compiler/AnalisadorLexico.py
+++ compiler/AnalisadorLexico.py
@@ -1,12 +1,10 @@
-from typing import NamedTuple, Union
-
 ERRO = 0
 IDENTIFICADOR = 1
 NUM_INT = 2
 NUM_REAL = 3
 EOS = 4
 
-PALAVRAS_RESERVADAS = ["begin", "boolean", "div", "do", "else", "end", "false", "if", "integer", "mod", "program", "read", "then", "true", "not", "var", "while",
+RESERVED_WORDS = ["begin", "boolean", "div", "do", "else", "end", "false", "if", "integer", "mod", "program", "read", "then", "true", "not", "var", "while",
 "write"]
 
 class Atomo(NamedTuple):
@@ -16,7 +14,7 @@ class Atomo(NamedTuple):
     line: int
 
 class AnalisadorLexico:
-    def __init__(self, source_code):
+    def _init_(self, source_code):
         self.code = source_code
         self.position = 0
         self.current_line = 1
@@ -29,10 +27,91 @@ class AnalisadorLexico:
             self.position += 1
 
         return char
+    
+    def retreat(self):
+        self.position -= 1
 
     def peek(self, i=0):
         index = self.position + i
         return self.code[index] if index < len(self.code) else None
+    
+    def next_atom(self):
+        self.skip_whitespace_and_comments()
+
+        char = self.peek()
+
+        if char is None:
+            return Atomo(EOS, "EOF", 0, self.current_line)
+        
+        if char.isalpha() or char == "_":
+            return self.handle_identifiers(char)
+        
+        if char.isdigit():
+            return self.handle_digits(char)
+    
+    def handle_identifiers(self, c):
+        lexema = c
+        c = self.advance()
+        stage = 1
+
+        while True:
+            if stage == 1:
+                if c.isalnum(): # letras ou dígitos
+                    lexema += c
+                    c = self.advance()
+                else:
+                    if lexema in RESERVED_WORDS:
+                        print("É uma palavra reservada")
+                        return Atomo(lexema.upper(), lexema.upper(), 0, self.current_line)
+                    stage = 2
+            elif stage == 2:
+                self.retreat()
+                return Atomo(IDENTIFICADOR, lexema, 0, self.current_line)
+
+
+    def handle_digits(self, c):
+        lexema = c
+        c = self.advance()
+        stage = 1
+
+        while True:
+            if stage == 1:
+                if c.isdigit():
+                    lexema += c
+                    c = self.advance()
+                elif c == '.':
+                    lexema += c
+                    stage = 3
+                    c = self.advance()
+                elif c.isalpha():
+                     return Atomo(ERRO, lexema, 0, self.current_line)
+                else:
+                    stage = 2 # É Inteiro
+
+            elif stage == 2:
+                self.retreat()
+                return Atomo(NUM_INT, lexema, int(lexema), self.current_line)
+
+            elif stage == 3: # Parte decimal
+                if c.isdigit():
+                    lexema += c
+                    stage = 4
+                    c = self.advance()
+                else:
+                    return Atomo(ERRO, lexema, 0, self.current_line)
+
+            elif stage == 4:
+                if c.isdigit():
+                    lexema += c
+                    c = self.advance()
+                elif c.isalpha():
+                    return Atomo(ERRO, lexema, 0, self.current_line)
+                else:
+                    stage = 5
+
+            elif stage == 5:
+                self.retreat()
+                return Atomo(NUM_REAL, lexema, float(lexema), self.current_line)
 
     def skip_whitespace_and_comments(self):
         char = self.code[self.position]
@@ -45,7 +124,6 @@ class AnalisadorLexico:
             
             if char in skip_chars:
                 self.advance()
-                advance = self.peek(1)
             elif [char, self.peek(1)] == ['(', '*']:
                 self._skip_star_comment()
             elif char == '{':
